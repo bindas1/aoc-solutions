d9b37b8d3f630da4fdc1b846d64e4d33455fc01e
unknown <bartek.binda46@gmail.com>
2023-12-26 11:32:33 +0100

    add days 6 and 13

---
+++ 13.py
@@ -0,0 +1,8 @@
+from aoc import get_input
+
+patterns = get_input(13).split('\n\n')
+
+for pattern in patterns:
+    pattern = pattern.split('\n')
+    print(pattern)
+    print(" --- ")
---
+++ 6.py
@@ -0,0 +1,6 @@
+from aoc import get_input
+
+data = get_input(6).splitlines()
+
+for line in data:
+    pass
